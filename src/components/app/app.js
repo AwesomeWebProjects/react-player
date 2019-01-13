13cdef9e8588e4f893557ddc78133c60afac27e4
Daniel Barion <danielbarionn@gmail.com>
2019-01-13 00:54:18 -0200

    Add github icon

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 // import HelloWorld from '../hello-world/component';
 import Audio from '../audio/component';
+import github from '../../assets/github/GitHub-Mark-Light-32px.png'
 
 class App extends Component {
   render() {
     // return [<HelloWorld />, <Audio />]
-    return <Audio />;
+    return [
+      <Audio key='audio-component' />,
+      <div key='github-logo'>
+        <a href='https://github.com/danielbarion/react-player' target='_alt' title='GitHub Repository'>
+          <img src={github} alt='GitHub' width='32' height='32' style={{'position': 'absolute', 'right': '2rem', 'bottom': '2rem'}}></img>
+        </a>
+      </div>
+    ]
   }
 }
 
