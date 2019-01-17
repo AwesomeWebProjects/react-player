4a75d8e8a607d75113269dc2cd5bd3c0a3a64170
Daniel Barion <daniel.barion@golsat.com.br>
2019-01-17 08:51:38 -0200

    [WIP] Add shortcut popup

--- src/components/footer-nav/component.js
+++ src/components/footer-nav/component.js
@@ -8,7 +8,17 @@ import {
 class footerNav extends Component {
 	constructor(props) {
 		super(props)
-			this.state = {}
+			this.state = {
+				actionIcons: [
+					{ name: 'shortcut', active: false }
+				]
+			}
+	}
+
+	switchActive(event) {
+		const element = event.currentTarget
+		element.className = 'active'
+		console.log(element)
 	}
 
 	/**
@@ -16,10 +26,17 @@ class footerNav extends Component {
 	*/
 	render() {
 		return (
-			<div className="footer-nav">
+			<div className='footer-nav'>
 				<ul>
-					<li>
+					<li onClick={this.switchActive}>
 						<Keyboard	style={{ fontSize: '32px' }} />
+						<div className='popup-shortcut'>
+							<ul>
+								<li><span>SpaceBar</span> Pause or resume song</li>
+								<li><span>N</span> Go to the next song</li>
+								<li><span>B</span> Back to the previous song</li>
+							</ul>
+						</div>
 					</li>
 					<li>
 						<div key='github-logo' data-for='githubTooltip' data-tip>
